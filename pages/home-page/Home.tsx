014f442e37b060a9a3dc40a97f3100676e15b0f2
patrick <patrick.lawrence@calrec.com>
2023-04-15 11:59:29 +0100

    setup: Add initial files

---
+++ pages/home-page/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button, Text } from "@react-native-material/core";
+
+export const Home = () => {
+  return (
+    <>
+      <Button title="Click Me" />
+      <Text>Open up App.js to start working on your app!</Text>
+    </>
+  );
+};
+
+export default Home;

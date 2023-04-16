1ca4f8f9f86376049838f701dcf84f9df915340e
patrick <patrick.lawrence@calrec.com>
2023-04-16 12:43:43 +0100

    feat: Make a start on the login

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StyleSheet, View } from "react-native";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  TouchableWithoutFeedback,
+} from "react-native";
 import Home from "./pages/home-page";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Home />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.select({
+          ios: () => -150,
+          android: () => 0,
+        })()}
+        style={styles.container}
+      >
+        <Home />
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -13,7 +28,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#ffffff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
--- pages/home-page/Home.tsx
+++ pages/home-page/Home.tsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { Button, Text } from "@react-native-material/core";
+import {
+  Button,
+  Flex,
+  Stack,
+  TextInput,
+  Wrap,
+} from "@react-native-material/core";
 
 export const Home = () => {
   return (
-    <>
-      <Button title="Click Me" />
-      <Text>Open up App.js to start working on your app!</Text>
-    </>
+    <Flex fill={1}>
+      <Stack fill={1} justify="center" spacing={12} p={12}>
+        <TextInput label="Username" variant="outlined" />
+        <TextInput label="Password" variant="outlined" />
+
+        <Wrap m={4} center spacing={12}>
+          <Button title="Log in" />
+          <Button title="Sign up" variant="outlined" />
+        </Wrap>
+      </Stack>
+    </Flex>
   );
 };
 
